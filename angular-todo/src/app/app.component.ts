8b33182c25e4811bc20a919e41942b04ba107e7b
sumanth <sumanthkoppula16@gmail.com>
2022-08-30 10:15:15 +0530

    first commit

---
+++ angular-todo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template:'<h1>{{title}}</h1>'
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-todo';
+  message = 'Welcome to Social Prachar';
+}
